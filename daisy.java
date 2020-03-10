1b371290a5a2cb2d655f8c94e9a6db2f75bbfa9c
j0hnchavez <39979644+j0hnchavez@users.noreply.github.com>
2020-03-10 15:51:38 -0700

    Add demo.java & daisy.java

---
+++ daisy.java
@@ -0,0 +1,11 @@
+package garden;
+import garden.*;
+
+public class daisy extends plant
+{
+	// Constructor for daisy
+	public daisy()
+	{
+		water_needed_setter(3);
+	}
+}
--- demo.java
+++ demo.java
@@ -1,15 +1,66 @@
 package garden;
 import garden.*;
+import java.util.Scanner;
 
 public class demo
 {
-	public static void main(String[] args)
+	public static void main (String[] args)
 	{
-		rose r = new rose();
-		System.out.println("Rose has been watered "+r.water_received_getter()+" times");
-		System.out.println("Roses need to be watered "+r.water_needed_getter()+" times");
-		System.out.println("Watering... (user will make this happen)");
-		r.water_this_plant();
-		System.out.println("Rose has been watered "+r.water_received_getter()+" times");
+		int day = 0;
+		// Garden initialization!
+		if(day==0)
+		{
+			// Entrance statements
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			System.out.println("Welcome to Gardenbot 2000!");
+			System.out.println("Today is your first day of being a virtual gardener.");
+			System.out.println("You can plant up to 20 different plants to start off your garden.");
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			System.out.println("");
+			
+			// Start off with 0 plants
+			int total_num=0;
+
+			// Prompt user for number of roses they'd like
+			// Their input is restricted so they can't exceed 20 plants total
+			// Create an array of rose inputs with a length of user input
+			// Throughout the project, the user's 5th rose will be referenced by r[4].
+			System.out.println("How many roses would you like?");
+			System.out.print("Roses >> ");
+			Scanner r_scan = new Scanner(System.in);
+			int r_num;
+			r_num = r_scan.nextInt();
+			while(r_num + total_num >  20)
+			{
+				System.out.println("Sorry, that's too many plants. You can only add "+(20-total_num)+" more plants. Please try again");
+				System.out.println("If you're already at 20 plants, enter 0 for the rest of the plant types.");
+				System.out.print("Roses >> ");
+				r_num = r_scan.nextInt();
+			}
+
+			// Update total number of plants
+			total_num = total_num + r_num;
+			System.out.println("(Total plants: "+total_num+") (Remaining spots: "+(20-total_num)+")");
+
+			// Same process but for daisies
+			System.out.println("How many daisies would you like?");
+			System.out.print("Daisies >> ");
+			Scanner d_scan = new Scanner(System.in);
+			int d_num;
+			d_num = d_scan.nextInt();
+			while(d_num + total_num >  20)
+			{
+				System.out.println("Sorry, that's too many plants. You can only add "+(20-total_num)+" more plants. Please try again");
+				System.out.println("If you're already at 20 plants, enter 0 for the rest of the plant types.");
+				System.out.print("Roses >> ");
+				d_num = d_scan.nextInt();
+			}
+
+			// Update total number of plants
+			total_num = total_num + d_num;
+			System.out.println("(Total plants: "+total_num+") (Remaining spots: "+(20-total_num)+")");
+
+	
+		}
 	}
 }
