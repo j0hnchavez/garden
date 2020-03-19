194ed55dd721d4557061eb9ea18a94fd657dc543
DinaZheng <dczheng33@gmail.com>
2020-03-19 03:27:24 -0700

    Updated plant.java

--- plant.java
+++ plant.java
@@ -11,18 +11,24 @@ public class plant
     // water_received
     // water_min
     // water_max
+    //fertilizer_min
+    //fertilizer_max
 
     private int water_received=0; // How many times this plant has been watered
     private int water_min; // Min water needed before DYING!!!!
     private int water_max; // Max water needed
     private int water_increment; // How much water goes up every day
     private boolean alive=true;
+    private int fertilizer_received=0; // How many times this plant has been fertilized
+    private int fertilizer_min; // Min fertilizer needed
+    private int fertilizer_max; // Max fertilizer needed
+    private int fertilizer_increment; // How much fertilizer goes up every day
     insect ist;
 
     /////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////             GETTERS             ////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////
-    
+
     // This function is a getter for water_received
     public int water_received_getter()
     {
@@ -46,16 +52,33 @@ public class plant
         return this.water_increment;
     }
 
-     // Return the life value of plant
-	public boolean life_getter()
-	{
-		return alive;
-	}
-    
-     /////////////////////////////////////////////////////////////////////////////////////
+    // Return the life value of plant
+    public boolean life_getter()
+    {
+        return alive;
+    }
+
+    //This function is a getter for fertilizer received
+    public int fertilizer_received_getter() {return this.fertilizer_received; }
+
+    // This function is a getter for fertilizer_min
+    public int fertilizer_min_getter()
+    {
+        return this.fertilizer_min;
+    }
+
+    // This function is a getter for fertilizer_max
+    public int fertilizer_max_getter()
+    {
+        return this.fertilizer_max;
+    }
+
+    //This function is a getter for how much water was incremented by
+    public int fertilizer_increment_getter() {return this.fertilizer_increment; }
+    /////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////             SETTERS             ////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////////
-    
+
 
     // This function is a setter for water_needed
     // Every plant type (rose, daisy, etc) just has to use this to define how much water it needs
@@ -82,10 +105,29 @@ public class plant
     {
         this.water_received = this.water_received + n;
     }
-	
-	// Set the life value of plant
-	public void life_setter(boolean a)
-	{
-		this.alive=a;
-	}
+
+    // Set the life value of plant
+    public void life_setter(boolean a)
+    {
+        this.alive=a;
+    }
+
+    //This function is a setter for fertilizer_min
+    // Every plant type (rose, daisy, etc) just has to use this to define how much fertilizer it needs
+    public void fertilizer_min_setter(int x)
+    {
+        this.fertilizer_min=x;
+    }
+
+    // This function is a setter for fertilizer_max
+    // Every plant type (rose, daisy, etc) just has to use this to define how much fertilizer it needs
+    public void fertilizer_max_setter(int x)
+    {
+        this.fertilizer_max=x;
+    }
+
+    public void fertilizer_increment_setter(int x) {this.fertilizer_increment=x; }
+
+    //This function fertilizes the plant n times. +N to fertilizer_received
+    public void fertilize_this_plant(int n) { this.fertilizer_received = this.water_received + n;}
 }
