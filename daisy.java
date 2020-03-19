702b806dce19d9ec3210d21841ed6bab74e39fbb
DinaZheng <dczheng33@gmail.com>
2020-03-19 03:28:15 -0700

    updated daisy.java
    added fertilizer setter and getters

--- daisy.java
+++ daisy.java
@@ -1,7 +1,7 @@
 package garden;
 import garden.*;
 
-import java.util.concurrent.ThreadLocalRandom;
+iimport java.util.concurrent.ThreadLocalRandom;
 
 public class daisy extends plant
 {
@@ -11,6 +11,8 @@ public class daisy extends plant
         ist = new insect();
         water_min_setter(ThreadLocalRandom.current().nextInt(0, 2));
         water_max_setter(ThreadLocalRandom.current().nextInt(3, 6));
-        water_increment_setter(2);
+        water_increment_setter(2); 
+        fertilizer_min_setter(ThreadLocalRandom.current().nextInt(1,4));
+        fertilizer_max_setter(ThreadLocalRandom.current().nextInt(5,8));
     }
 }
